82b516f5af863b215e339d96668289d0328e7de4
Jakob Jaeger <JakobJaeger@protonmail.com>
2023-06-04 13:07:06 +0200

    import test

--- game/src/Main.java
+++ game/src/Main.java
@@ -1,37 +1,39 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+import spritelib.Point;
+import spritelib.SingleSprite;
+/*
 public class Main {
     public static void main(String[] args){PApplet.main("invaderlib.Window");}
 }
+*/
 
-/*
-public class Main extends PApplet
-{
-    public static void main( String[] args )
-    {
-        // start the processing-application
-        PApplet.main( Main.class );
-    }
 
-    /**
-     * settings
-     * sets the window-size
-     */
-/*
-    @Override
-    public void settings()
+    public class Main extends PApplet
     {
-        setSize( 800, 600 );
+        PImage ballImage;
+        SingleSprite aSingleSprite;
+        Point drawpoint = new Point( 100, 100 );
+        public static void main( String[] args )
+        {
+            PApplet.main( Main.class );
+        }
+        @Override
+        public void settings()
+        {
+            setSize( 800, 600 );
+        }
+        @Override
+        public void setup()
+        {
+            super.setup();
+            ballImage = loadImage( "ball.png" );
+            aSingleSprite = new SingleSprite( ballImage );
+        }
+        @Override
+        public void draw()
+        {
+            background( 255 );
+            aSingleSprite.draw( this, drawpoint );
+        }
     }
-
-    @Override
-    public void setup()
-    {
-        super.setup();
-    }
-
-    @Override
-    public void draw()
-    {
-        background( 255 );
-    }
-}*/
